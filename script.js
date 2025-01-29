a4caa303f9bc5d4b02e50bed1b62245d1327e079
Iasmym <iasmympatricia7@gmail.com>
2025-01-29 19:44:08 -0300

    paramemetro

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ console.log(nome);
 // argumento 
 // valor que eu irei passar
 exibirIdade(iasmym);
+
+// porque utilizar parametros
+// para utilizar a mesma funçao para valores diferentes
+
+
+function somar(numero1,numero2){
+
+    console.log(numero1 + numero2)
+
+}
+
+somar(10,7);
+somar(17, 89);
+
