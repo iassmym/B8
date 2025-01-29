bbf7d15e5f366815b440a440db970ba90b372431
Iasmym <iasmympatricia7@gmail.com>
2025-01-29 19:24:09 -0300

    mudança 1

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome= "tabs";
+
+//bloco de codigos( // codigo )
+function exibirIdade(){
+    //variavel de escopo local
+    var idade  = 24
+    console.log(idade);
+    
+}
+
+console.log(nome);
+exibirIdade()
